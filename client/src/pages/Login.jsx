920264bd2a07c553bf886e417ba4ae45a7b5c338
Nevil Patel <93375041+NEVIL-PATEL@users.noreply.github.com>
2023-04-30 21:48:29 +0530

    client

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className="auth">
+            <h1>Login</h1>
+            <form action="">
+                <input type="text" name="" id="" placeholder='UserName' />
+                <input type="password" name="" id="" placeholder='Password' />
+                <button>Login</button>
+            </form>
+        </div>)
+}
+
+export default Login
