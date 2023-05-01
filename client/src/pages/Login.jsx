98e04b79990575aeb8e23c296423bc87169735ac
Nevil Patel <93375041+NEVIL-PATEL@users.noreply.github.com>
2023-05-01 14:17:43 +0530

    login/register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Login = () => {
     return (
         <div className="auth">
             <h1>Login</h1>
             <form action="">
-                <input type="text" name="" id="" placeholder='UserName' />
-                <input type="password" name="" id="" placeholder='Password' />
+                <input required type="text" name="" id="" placeholder='UserName' />
+                <input required type="password" name="" id="" placeholder='Password' />
                 <button>Login</button>
+                <p>This is an error!</p>
+                <span>Dont't you have an account? <Link style={{ textDecoration: "none" }} to='/register'>Register</Link></span>
             </form>
-        </div>)
+        </div>
+    )
 }
 
 export default Login
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="auth">
+            <h1>Register</h1>
+            <form action="">
+                <input required type="text" name="" id="" placeholder='UserName' />
+                <input required type="email" name="" id="" placeholder='Email' />
+                <input required type="password" name="" id="" placeholder='Password' />
+                <button>Login</button>
+                <p>This is an error!</p>
+                <span>Do you have an account? <Link style={{ textDecoration: "none" }} to='/login'>Login</Link></span>
+            </form>
+        </div>
   )
 }
 
